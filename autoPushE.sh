87fed2caa33a2fb078c628fd2c81027e089ea16b
nsavinda <nirmalsavinda29@gmail.com>
2023-01-29 16:54:35 +0530

    list add remove

--- autoPushE.sh
+++ autoPushE.sh
@@ -27,3 +27,38 @@ fi
 
 
 
+if [ $1 == 'd' ]
+then 
+    if [ -n "$2" ]
+    then
+    c=0
+    n=0
+    while IFS= read -r line
+    do
+        n=$(($n+1))
+        if [[ $line != \#* ]]
+        then
+            c=$(($c+1))
+            if [ $c -eq $2 ]
+            then
+                # echo "Are you sure delete entry " $line
+                # read -p "Are you sure delete entry  $line  (y/n)?" -n 1 -r
+                echo $line
+                echo "\"$line\"d"
+                
+                # sed "\"$line\"d" < autoPush.config
+                sed -i "s#$line##" autoPush.config
+                sed -i '/^$/d' autoPush.config
+
+                # if [ $o == 'y' ]
+                # then
+                #     echo '"${line}"d'
+                # fi
+            fi
+
+            # echo $c $line
+        fi
+    done < $input
+    fi
+
+fi
