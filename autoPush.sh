9829dd48f8355f6c3b5d960e7d2dad0ec6389940
nsavinda <nirmalsavinda29@gmail.com>
2023-02-05 20:53:42 +0530

    new

--- autoPush.sh
+++ autoPush.sh
@@ -1,23 +1,88 @@
 #!/bin/bash
-input="./autoPush.config"
+dataFile="./autoPush.data"
+gitPath=$(which git)
 
-gitp=$(which git)
-# echo $gitp
 
-while IFS= read -r line
-do
-    if [[ $line != \#* ]]
+if [ $# -eq 0 ]
+then 
+    while IFS= read -r line
+    do
+        if [[ $line != \#* && "$line" != ""  ]]
+        then
+            path=$(awk '{print $1}' FS=';' <<< $line)
+            com_msg=$(awk '{print $2}' FS=';' <<< $line)
+            echo $path
+            echo $com_msg
+            cd $path && $gitPath add .
+            cd $path && $gitPath commit -m "$com_msg"
+            cd $path && $gitPath push
+            date >> $path/autopush.log
+            echo $(date) ":: Pushed $path" >> autopush.log
+        fi
+    done < "$dataFile"
+    exit
+fi
+
+
+if [[ $1 == 'l' || $1 == "-l" ]]
+then
+    echo $1
+    c=0
+    while IFS= read -r line
+    do
+
+         if [[ $line != \#* && "$line" != "" ]]
+         then
+            c=$(($c+1))
+            echo $c $line
+         fi
+    done < $dataFile
+
+
+elif [[ $1 == 'a' || $1 == "-a" ]]
+then
+    read -p "Enter path: " -r path
+    read -p "Commit message: " -r msg
+    echo $path';' '"'$msg'"' >> $dataFile
+
+
+
+
+elif [[ $1 == 'd' || $1 == "-d" ]]
+then 
+    if [ -n "$2" ]
     then
-        path=$(awk '{print $1}' FS=';' <<< $line)
-        com_msg=$(awk '{print $2}' FS=';' <<< $line)
-        echo $path
-        echo $com_msg
-        cd $path && $gitp add .
-        cd $path && $gitp commit -m "$com_msg"
-        cd $path && $gitp push
-        date >> $path/autopush.log
-        # echo "$line"
-    # else
-    #     echo "$line"
+    c=0
+    n=0
+    while IFS= read -r line
+    do
+        n=$(($n+1))
+        if [[ $line != \#* && "$line" != "" ]]
+        then
+            c=$(($c+1))
+            if [ $c -eq $2 ]
+            then
+                # echo "Are you sure delete entry " $line
+                read -p "Are you sure delete entry  $line  (y/n)?" o </dev/tty
+                echo $o
+                if [[ $o == 'y' ||  $o == 'Y' ]]
+                then
+                # echo $line
+                # echo "\"$line\"d"
+                
+                # sed "\"$line\"d" < autoPush.config
+                    sed -i "s#$line##" $dataFile
+                    sed -i '/^$/d' $dataFile
+                fi
+                # if [ $o == 'y' ]
+                # then
+                #     echo '"${line}"d'
+                # fi
+            fi
+
+            # echo $c $line
+        fi
+    done < $dataFile
     fi
-done < "$input"
+
+fi
